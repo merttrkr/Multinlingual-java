5fd13fef4d6508cf6b75279b85480bb526761aa4
Mert Türker <mertturker99@gmail.com>
2023-04-04 22:30:02 +0300

    added random unit generator

--- src/Language.java
+++ src/Language.java
@@ -1,14 +1,17 @@
-
+import java.util.Random;
 public abstract class Language {
 	private String name;
-	private String[] unitsArray;
+	private Unit[] unitsArray;
 	private String[] leagueArray;
-		
-    public Language(String name, String[] unitsArray, String[] leagueArray) {
+	private int unitNumber;
+
+    public Language(String name ) {// only name needed when generating this class.
     	this.name = name;
-        this.unitsArray = unitsArray;
-        this.leagueArray = leagueArray;
+        
     }
+    public int getUnitNumber() {
+		return unitNumber;
+	}
 	
 	public String getName() {
 		return name;
@@ -26,12 +29,20 @@ public abstract class Language {
 		this.leagueArray = leagueArray;
 	}
 
-	public String[] getUnitsArray() {
+	public Unit[] getUnitsArray() {
 		return unitsArray;
 	}
+	
+	public void createUnits() {
+		Random rand = new Random();
+        int numUnits = rand.nextInt(41) + 60; // generates a random number between 60 and 100
+        this.unitNumber = numUnits;
+        
+        for(int i= 0; i < numUnits ; i ++) {
+        	Unit unit = new Unit();
+        	this.unitsArray[i] = unit;
+        }
 
-	public void setUnitsArray(String[] unitsArray) {
-		this.unitsArray = unitsArray;
 	}
 	
 }
